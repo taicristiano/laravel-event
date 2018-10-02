2282879933a11508e609d6a4ba86dc6ca704bf20
Tai Cristiano <taductaik56a3@gmail.com>
2018-10-02 10:50:11 +0700

    Make auth

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/test-event', [
     'uses' => 'EventController@testEvent',
     'as' => 'test-event',
 ]);
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
