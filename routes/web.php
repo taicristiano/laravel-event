5a41faf6be85513780008bfbb4d3a95dc3c2aba1
Tai Cristiano <taductaik56a3@gmail.com>
2019-03-01 10:33:00 +0700

    Route laravel and react

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::view('/product/{path?}', 'welcome')
+     ->where('path', '.*')
+     ->name('react');
